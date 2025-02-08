986fe0dcf9ffa9d159febd32fedcd399a2ff4312
dagike <isaac20053@hotmail.com>
2025-02-08 14:19:23 -0500

    add: class LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,10 +9,12 @@ class HTMLNode:
     NotImplementedError
 
   def props_to_html(self):
+    if(not self.props):
+      return ""
     return " ".join(f'{key}="{value}"' for key, value in self.props.items())
 
   def __repr__(self):
-    return f"HTMLNode(tag={self.tag} value={self.value} children={self.children} props={self.props})"
+    return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
   
   def __eq__(self, other):
     return (
@@ -21,3 +23,16 @@ class HTMLNode:
       and self.children == other.children
       and self.props == other.props
     )
+  
+class LeafNode(HTMLNode):
+  def __init__(self, tag, value, props=None):
+    super().__init__(tag, value, None, props)
+
+  def to_html(self):
+    if(not self.value):
+      raise ValueError("All leaf nodes must have a value")
+    if(not self.tag):
+      return self.value
+    if(not self.props):
+      return f"<{self.tag}>{self.value}</{self.tag}>"
+    return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
