4aa1ec63c23cdfab387a054454c110d4b4e5dfa2
dagike <isaac20053@hotmail.com>
2025-02-10 18:18:05 -0500

    add: markdown to html conversion logic

--- src/textnode.py
+++ src/textnode.py
@@ -23,7 +23,9 @@ class TextNode:
     )
   
   def __repr__(self):
-    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    if (not self.url):
+      return f"TextNode(\"{self.text}\", {self.text_type.value})"
+    return f"TextNode(\"{self.text}\", {self.text_type.value}, {self.url})"
 
 def text_node_to_html_node(text_node):
   match(text_node.text_type):
